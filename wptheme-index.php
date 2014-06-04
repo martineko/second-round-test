8da7d6c10335a078fec34c190ab7f096a129efe1
martineko <martin.e.setia@gmail.com>
2014-06-05 02:23:02 +0700

    Update wptheme-index.php

--- wptheme-index.php
+++ wptheme-index.php
@@ -9,9 +9,9 @@
             <li data-target="#mycarousel" data-slide-to="1"></li>
           </ol>
           <div class="carousel-inner">
-            <div class="item active"> <a href="#" target="_blank"><img src="../wp-content/themes/HWZlaw/assets/img/slide1.jpg" class="img-responsive" style="width:100%;"></a> </div>
-            <div class="item"> <a href="#" target="_blank"><img src="../wp-content/themes/HWZlaw/assets/img/slide2.jpg"  class="img-responsive" style="width:100%;"></a> </div>
-            <div class="item"> <a href="#" target="_blank"><img src="../wp-content/themes/HWZlaw/assets/img/slide3.jpg"  class="img-responsive" style="width:100%;"></a> </div>
+            <div class="item active"> <a href="#" target="_blank"><img src="<?php bloginfo('template_url');?>/assets/img/slide1.jpg" class="img-responsive" style="width:100%;"></a> </div>
+            <div class="item"> <a href="#" target="_blank"><img src="<?php bloginfo('template_url');?>/assets/img/slide2.jpg"  class="img-responsive" style="width:100%;"></a> </div>
+            <div class="item"> <a href="#" target="_blank"><img src="<?php bloginfo('template_url');?>/assets/img/slide3.jpg"  class="img-responsive" style="width:100%;"></a> </div>
           </div>
         </div>
       </div>
@@ -19,17 +19,17 @@
         <div class="menu">
           <div class="middle-menu">
             <ul id="menu-middle-menu" class="middle-nav">
-              <li id="menu-item-1" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-1">
-                <div id="cufon-area-rightmenu"> <a href="#">INDIVIDUAL BANKRUPTCY</a> </div>
+              <li id="menu-item-1" class="menu-item-1">
+                <div id="cufon-area-rightmenu"> <a href="../about-us">INDIVIDUAL BANKRUPTCY</a> </div>
               </li>
-              <li id="menu-item-2" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-2">
-                <div id="cufon-area-rightmenu"><a href="#">BUSSINES BANKRUTCY</a></div>
+              <li id="menu-item-2" class="menu-item-2">
+                <div id="cufon-area-rightmenu"><a href="../about-us">BUSSINES BANKRUTCY</a></div>
               </li>
-              <li id="menu-item-3" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-3">
-                <div id="cufon-area-rightmenu"><a href="#">COMERCIAL LIGITATION</a></div>
+              <li id="menu-item-3" class="menu-item-3">
+                <div id="cufon-area-rightmenu"><a href="../about-us">COMERCIAL LIGITATION</a></div>
               </li>
-              <li id="menu-item-4" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-4">
-                <div id="cufon-area-rightmenu"><a href="#">TAXZYTION</a></div>
+              <li id="menu-item-4" class="menu-item-4">
+                <div id="cufon-area-rightmenu"><a href="../about-us">TAXZYTION</a></div>
               </li>
             </ul>
           </div>
@@ -49,15 +49,15 @@
   
   <section class="Person">
     <div class="row">
-      <div class="col-lg-4"> <img src="../wp-content/themes/HWZlaw/assets/img/John.png" width="100%" class="img-responsive">
+      <div class="col-lg-4"> <img src="<?php bloginfo('template_url');?>/assets/img/John.png" width="100%" class="img-responsive">
         <div id="cufon-area-personname">JOHN F. HILTZ</div>
         <p  align="justify"> John Hiltz has experience representing financial institutions, as well as a wide range of secured 	and unsecured creditors in business reorganizations and bankruptcy matters. John Hiltz has also represented consumer debtors in complex Chapter 7 and 13 filings. </p>
       </div>
-      <div class="col-lg-4"> <img src="../wp-content/themes/HWZlaw/assets/img/Kathy.png" width="100%" class="img-responsive">
+      <div class="col-lg-4"> <img src="<?php bloginfo('template_url');?>/assets/img/Kathy.png" width="100%" class="img-responsive">
         <div id="cufon-area-personname">KATHY WANTCUH</div>
         <p align="justify"> Kathy Wantuch focuses primarily in the area of consumer and business bankruptcy. She earned her undergraduate degree in accounting at the University of Illinois in 2003, with Honors. Kathy Wantuch graduated cum laude from The John Marshall Law School in 2007, where she served on The John Marshall Law Review. </p>
       </div>
-      <div class="col-lg-4"> <img src="../wp-content/themes/HWZlaw/assets/img/Blair.png" width="100%" class="img-responsive">
+      <div class="col-lg-4"> <img src="<?php bloginfo('template_url');?>/assets/img/Blair.png" width="100%" class="img-responsive">
         <div id="cufon-area-personname">BLAIR ZANZIG</div>
         <p align="justify"> John Hiltz has experience representing financial institutions, as well as a wide range of secured and unsecured creditors in business reorganizations and bankruptcy matters. John Hiltz has also represented consumer debtors in complex Chapter 7 and 13 filings. </p>
       </div>
