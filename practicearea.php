915d86af8f79678238c800deb4d5d71e85c61f12
martineko <martin.e.setia@gmail.com>
2014-06-05 01:54:49 +0700

    Create practicearea.php

---
+++ practicearea.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>HWZ Law</title>
+<link href="assets/css/bootstrap.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+<link href="assets/css/superfish.css" rel="stylesheet">
+</head>
+
+<body>
+<header style="background-color:#A64029;">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 header">
+        <div class="top-menu">
+          <ul id="menu-top-menu" class="sf-menu top-nav">
+            <li id="menu-item-1" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-1 current_page_item menu-item-1"><a href="index.php">Home</a></li>
+            <li id="menu-item-2" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2"><a href="#">About Us</a></li>
+            <li id="menu-item-3" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3"><a href="#">ATTORNEYS</a>
+              <ul class="sub-menu">
+                <li id="menu-item-4" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-4"><a href="#">John F. Hiltz</a></li>
+                <li id="menu-item-5" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-5"><a href="#">Kathy Wantuch</a></li>
+                <li id="menu-item-6" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-6"><a href="#">Blair R. Zanzig</a></li>
+              </ul>
+            </li>
+            <li id="menu-item-7" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-7"><a href="#">Practice Areas</a>
+              <ul class="sub-menu">
+                <li id="menu-item-8" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-8"><a href="#">COMMERCIAL BANKRUPTCY</a></li>
+                <li id="menu-item-9" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-9"><a href="#">COMMERCIAL LITIGATION</a></li>
+                <li id="menu-item-10" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-10"><a href="#">INDIVIDUAL BANKRUPTCY</a></li>
+                <li id="menu-item-11" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-11"><a href="#">TAXZYTION</a></li>
+              </ul>
+            </li>
+            <li id="menu-item-12" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-12"><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+
+<div class="container">
+
+  <section class="Logo">
+    <div class="row">
+      <div class="col-lg-2"> <img src="assets/img/HWZ-Logo.png" class="img-responsive"> </div>
+      <div class="col-lg-10 no-gutter-text-logo"><br>
+        <div id="cufon-area-logotext">HILTZ WANTUCH & ZANZIG LLC</div>
+      </div>
+    </div>
+  </section>
+  
+  <section class="garisbatas">
+  <div class="row">
+  	<div class="col-lg-12">
+    	<hr class="garis">
+    </div>
+  </div>
+  </section>
+  
+  <section class="Content">
+  		<div class="row">
+        	<div class="col-lg-4">
+           		<div class="box-linkname">
+            		INDIVIDUAL BANKRUTCY
+           		 </div>
+            	<img src="assets/img/INDIVIDUAL BANKRUPTCY.png" style="width:100%">
+          
+            	<br>
+            	<div class="box-linkname">
+            		BUSINESS BANKRUTCY
+           		 </div>
+           		 <img src="assets/img/Business Bankruptcy.png" style="width:100%">
+                 <br>
+                 <div class="box-linkname">
+            		COMMERCIAL LIGITATION
+           		 </div>
+           		 <img src="assets/img/COMMERCIAL LITIGATION.png" style="width:100%">
+             </div>
+            <div class="col-lg-5" style="text-align:justify;vertical-align:text-top;">
+            	<h3>Legal Financial Solutions for Business</h3>
+            	<p align="justify" style="font-size:14px; font-family:Arial, Helvetica, sans-serif;">
+	Hiltz & Wantuch LLC is a specialized law firm that focuses its practice on bankruptcy related matters. John Hiltz and Kathy 			Wantuch formed Hiltz & Wantuch LLC in early 2010 to use their unique set of skills and experience to better serve their clients. John and Kathy began their careers within the Bankruptcy Court system here in Chicago. They both worked directly under a Judge as law clerks in the Bankruptcy Court for the Northern District of Illinois. Since then they have focused their legal careers in the field of bankruptcy law. This experience enables Hiltz & Wantuch LLC to successfully navigate the Bankruptcy Court and provide effective legal solutions to its clients.</p>
+ 				<p align="justify" style="font-size:14px; font-family:Arial, Helvetica, sans-serif;">
+Whether you are an individual contemplating bankruptcy, a small business facing possible liquidation or reorganization, or a creditor looking to retain a lawyer to advocate for your rights, choose Hiltz & Wantuch LLC.
+</p> 
+ 				<p align="justify" style="font-size:14px; font-family:Arial, Helvetica, sans-serif;">
+Hiltz & Wantuch LLC prides itself on exceptional customer service and zealous advocacy. We offer free initial consultations and offer individualized fee arrangements at competitive prices.  Evening and weekend hours are also available by appointment. Call or email Hiltz & Wantuch LLC today to discuss </p>
+            </div>
+            <div class="col-lg-3">
+            <div class="box-linkname" style="text-align:left;">
+            	CONTACT US
+            </div>
+            	<address?>HWZ LAW<br>
+				53 West Jackson<br>
+				Suite 205<br>
+				Chicago, IL 60604<br>
+				Phone: 312.566.9008<br>
+				Fax:312.5669015<br>
+				Email: <a href="mailto:#">info@hwzlaw.com</a>
+                </address>
+            </div>
+       	</div>
+  </section>
+ </div>
+ 
+ 
+ 
+<footer style="background-color:#404041; min-height:30px;" >
+<div class="container">
+ <div class="row">
+      <div class="col-lg-8">
+       <div class="footer-left"> 
+      @2013. Hiltz & Wantuch LLC. All right reserved. We are a debt relief agency. we help people file for bankrutcy relief under the bankrutcy code. </div>
+      </div>
+      <div class="col-lg-4"> 
+       <div class="footer-right"> Website design by Fresh Design Studios</div>
+      </div>
+      
+    </div>
+ </div>
+</footer>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="assets/js/jquery-1.11.1.js"></script> 
+<script src="assets/js/bootstrap.js"></script> 
+<script src="assets/js/cufon-yui.js"></script> 
+<script src="assets/js/Trajan_Pro_400-Trajan_Pro_700.font.js"></script> 
+<script src="assets/js/superfish.js"></script> 
+<script src="assets/js/hoverIntent.js"></script> 
+<script>
+		jQuery(document).ready(function(){
+			jQuery('ul.sf-menu').superfish();
+		});
+</script> 
+<script type="text/javascript">
+			Cufon.replace('#cufon-area-logotext, #cufon-area-personname, #cufon-area-rightmenu');
+    </script>
+</body>
+</html>
