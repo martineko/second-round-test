45df86dcea9fb7b9a1c666041e26be31010ef116
martineko <martin.e.setia@gmail.com>
2014-06-05 02:22:03 +0700

    Update wptheme-header.php

--- wptheme-header.php
+++ wptheme-header.php
@@ -5,8 +5,13 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>HWZ Law</title>
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-<link rel="shortcut icon" href=""assets/img/hwz_ico.ico" />
+
+<link rel="stylesheet" href="<?php bloginfo('template_url');?>/assets/css/bootstrap.css">
+<link rel="stylesheet" href="<?php bloginfo('template_url');?>/assets/css/bootstrap-responsive.css"> 
+<link rel="stylesheet" href="<?php bloginfo('template_url');?>/assets/css/superfish.css">
+<link rel="stylesheet" href="<?php bloginfo('template_url');?>/style.css">
+
+<link rel="shortcut icon" href="<?php bloginfo('template_url');?>assets/img/hwz_ico.ico" />
 </head>
 
 <body>
@@ -16,24 +21,24 @@
       <div class="col-lg-12 header">
         <div class="top-menu">
           <ul id="menu-top-menu" class="sf-menu top-nav">
-            <li id="menu-item-1" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-1 current_page_item menu-item-1"><a href="http://roemamayda.webuda.com/index.php">Home</a></li>
-            <li id="menu-item-2" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2"><a href="http://roemamayda.webuda.com/About-us">About Us</a></li>
-            <li id="menu-item-3" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3"><a href="#">ATTORNEYS</a>
+            <li id="menu-item-1" class="menu-item-1"><a href="../index.php">Home</a></li>
+            <li id="menu-item-2" class="menu-item-2"><a href="../About-us">About Us</a></li>
+            <li id="menu-item-3" class="menu-item-3"><a href="#">ATTORNEYS</a>
               <ul class="sub-menu">
-                <li id="menu-item-4" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-4"><a href="#">John F. Hiltz</a></li>
-                <li id="menu-item-5" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-5"><a href="#">Kathy Wantuch</a></li>
-                <li id="menu-item-6" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-6"><a href="#">Blair R. Zanzig</a></li>
+                <li id="menu-item-4" class="menu-item-4"><a href="#">John F. Hiltz</a></li>
+                <li id="menu-item-5" class="menu-item-5"><a href="#">Kathy Wantuch</a></li>
+                <li id="menu-item-6" class="menu-item-6"><a href="#">Blair R. Zanzig</a></li>
               </ul>
             </li>
-            <li id="menu-item-7" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-7"><a href="#">Practice Areas</a>
+            <li id="menu-item-7" class="menu-item-7"><a href="#">Practice Areas</a>
               <ul class="sub-menu">
-                <li id="menu-item-8" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-8"><a href="#">COMMERCIAL BANKRUPTCY</a></li>
-                <li id="menu-item-9" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-9"><a href="#">COMMERCIAL LITIGATION</a></li>
-                <li id="menu-item-10" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-10"><a href="#">INDIVIDUAL BANKRUPTCY</a></li>
-                <li id="menu-item-11" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-11"><a href="#">TAXZYTION</a></li>
+                <li id="menu-item-8" class="menu-item-8"><a href="../About-us">COMMERCIAL BANKRUPTCY</a></li>
+                <li id="menu-item-9" class="menu-item-9"><a href="../About-us">COMMERCIAL LITIGATION</a></li>
+                <li id="menu-item-10" class="menu-item-10"><a href="../About-us">INDIVIDUAL BANKRUPTCY</a></li>
+                <li id="menu-item-11" class="menu-item-11"><a href="../About-us">TAXZYTION</a></li>
               </ul>
             </li>
-            <li id="menu-item-12" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-12"><a href="#">Contact</a></li>
+            <li id="menu-item-12" class="menu-item-12"><a href="#">Contact</a></li>
           </ul>
         </div>
       </div>
@@ -46,7 +51,7 @@
 
   <section class="Logo">
     <div class="row">
-      <div class="col-lg-2"> <img src="../wp-content/themes/HWZlaw/assets/img/HWZ-Logo.png" class="img-responsive"> </div>
+      <div class="col-lg-2"> <img src="<?php bloginfo('template_url');?>/assets/img/HWZ-Logo.png" class="img-responsive"> </div>
       <div class="col-lg-10 no-gutter-text-logo"><br>
         <div id="cufon-area-logotext">HILTZ WANTUCH & ZANZIG LLC</div>
       </div>
