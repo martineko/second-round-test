a83a3043085615006f0064af3c82050f9cf3adbe
martineko <martin.e.setia@gmail.com>
2014-06-05 02:10:42 +0700

    Create wptheme-header.php

---
+++ wptheme-header.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>HWZ Law</title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+<link rel="shortcut icon" href=""assets/img/hwz_ico.ico" />
+</head>
+
+<body>
+<header style="background-color:#A64029;">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 header">
+        <div class="top-menu">
+          <ul id="menu-top-menu" class="sf-menu top-nav">
+            <li id="menu-item-1" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-1 current_page_item menu-item-1"><a href="http://roemamayda.webuda.com/index.php">Home</a></li>
+            <li id="menu-item-2" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2"><a href="http://roemamayda.webuda.com/About-us">About Us</a></li>
+            <li id="menu-item-3" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3"><a href="#">ATTORNEYS</a>
+              <ul class="sub-menu">
+                <li id="menu-item-4" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-4"><a href="#">John F. Hiltz</a></li>
+                <li id="menu-item-5" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-5"><a href="#">Kathy Wantuch</a></li>
+                <li id="menu-item-6" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-6"><a href="#">Blair R. Zanzig</a></li>
+              </ul>
+            </li>
+            <li id="menu-item-7" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-7"><a href="#">Practice Areas</a>
+              <ul class="sub-menu">
+                <li id="menu-item-8" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-8"><a href="#">COMMERCIAL BANKRUPTCY</a></li>
+                <li id="menu-item-9" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-9"><a href="#">COMMERCIAL LITIGATION</a></li>
+                <li id="menu-item-10" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-10"><a href="#">INDIVIDUAL BANKRUPTCY</a></li>
+                <li id="menu-item-11" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-11"><a href="#">TAXZYTION</a></li>
+              </ul>
+            </li>
+            <li id="menu-item-12" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-12"><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+
+<div class="container">
+
+  <section class="Logo">
+    <div class="row">
+      <div class="col-lg-2"> <img src="../wp-content/themes/HWZlaw/assets/img/HWZ-Logo.png" class="img-responsive"> </div>
+      <div class="col-lg-10 no-gutter-text-logo"><br>
+        <div id="cufon-area-logotext">HILTZ WANTUCH & ZANZIG LLC</div>
+      </div>
+    </div>
+  </section>
+  
+  
+  <section class="Line">
+    <div class="row">
+      <div class="col-lg-12">
+        <hr class="garis">
+      </div>
+    </div>
+  </section>
+  
