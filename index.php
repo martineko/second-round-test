af4ee1e6b4258b778e867634345c900ea67325f3
martineko <martin.e.setia@gmail.com>
2014-06-05 01:49:58 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>HWZ Law</title>
+<link href="../assets/css/bootstrap.css" rel="stylesheet">
+<link href="../style.css" rel="stylesheet">
+<link href="../assets/css/superfish.css" rel="stylesheet">
+<link rel="shortcut icon" href="../img/hwz_ico.ico" />
+</head>
+
+<body>
+<header style="background-color:#A64029;">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 header">
+        <div class="top-menu">
+          <ul id="menu-top-menu" class="sf-menu top-nav">
+            <li id="menu-item-1" class="menu-item-1"><a href="../index.php">Home</a></li>
+            <li id="menu-item-2" class="menu-item-2"><a href="#">About Us</a></li>
+            <li id="menu-item-3" class="menu-item-3"><a href="#">ATTORNEYS</a>
+              <ul class="sub-menu">
+                <li id="menu-item-4" class="menu-item-4"><a href="#">John F. Hiltz</a></li>
+                <li id="menu-item-5" class="menu-item-5"><a href="#">Kathy Wantuch</a></li>
+                <li id="menu-item-6" class="menu-item-6"><a href="#">Blair R. Zanzig</a></li>
+              </ul>
+            </li>
+            <li id="menu-item-7" class="menu-item-7"><a href="#">Practice Areas</a>
+              <ul class="sub-menu">
+                <li id="menu-item-8" class="menu-item-8"><a href="../practicearea.php">COMMERCIAL BANKRUPTCY</a></li>
+                <li id="menu-item-9" class="menu-item-9"><a href="../practicearea.php">COMMERCIAL LITIGATION</a></li>
+                <li id="menu-item-10" class="menu-item-10"><a href="../practicearea.php">INDIVIDUAL BANKRUPTCY</a></li>
+                <li id="menu-item-11" class="menu-item-11"><a href="../practicearea.php">TAXZYTION</a></li>
+              </ul>
+            </li>
+            <li id="menu-item-12" class="menu-item-12"><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+
+<div class="container">
+
+  <section class="Logo">
+    <div class="row">
+      <div class="col-lg-2"> <img src="../assets/img/HWZ-Logo.png" class="img-responsive"> </div>
+      <div class="col-lg-10 no-gutter-text-logo"><br>
+        <div id="cufon-area-logotext">HILTZ WANTUCH & ZANZIG LLC</div>
+      </div>
+    </div>
+  </section>
+  
+  
+  <section class="garisbatas">
+    <div class="row">
+      <div class="col-lg-12">
+        <hr class="garis">
+      </div>
+    </div>
+  </section>
+  
+  
+  <section class="menu">
+    <div class="row no-gutter">
+      <div class="col-lg-8">
+        <div id="mycarousel" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#mycarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#mycarousel" data-slide-to="1"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="item active"> <a href="#" target="_blank"><img src="../assets/img/slide1.jpg" class="img-responsive" style="width:100%;"></a> </div>
+            <div class="item"> <a href="#" target="_blank"><img src="../assets/img/slide2.jpg"  class="img-responsive" style="width:100%;"></a> </div>
+            <div class="item"> <a href="#" target="_blank"><img src="../assets/img/slide3.jpg"  class="img-responsive" style="width:100%;"></a> </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="menu">
+          <div class="middle-menu">
+            <ul id="menu-middle-menu" class="middle-nav">
+              <li id="menu-item-1" class="menu-item-1">
+                <div id="cufon-area-rightmenu"> <a href="../practicearea.php">INDIVIDUAL BANKRUPTCY</a> </div>
+              </li>
+              <li id="menu-item-2" class="menu-item-2">
+                <div id="cufon-area-rightmenu"><a href="../practicearea.php">BUSSINES BANKRUTCY</a></div>
+              </li>
+              <li id="menu-item-3" class="menu-item-3">
+                <div id="cufon-area-rightmenu"><a href="../practicearea.php">COMERCIAL LIGITATION</a></div>
+              </li>
+              <li id="menu-item-4" class="menu-item-4">
+                <div id="cufon-area-rightmenu"><a href="../practicearea.php">TAXZYTION</a></div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  
+  <section class="middle-text">
+    <div class="row">
+      <div class="col-lg-4 text-middle2"> <div id="cufon-area-logo21">CUSTOMIZED ADVICE </div></div>
+      <div class="col-lg-8 text-middle2"><div id="cufon-area-logo21">LEGAL FINANCIAL SOLUTION FOR BUSSINES </div></div>
+    </div>
+  </section>
+  
+  
+  <section class="Person">
+    <div class="row">
+      <div class="col-lg-4"> <img src="../assets/img/John.png" width="100%" class="img-responsive">
+        <div id="cufon-area-personname">JOHN F. HILTZ</div>
+        <p  align="justify"> John Hiltz has experience representing financial institutions, as well as a wide range of secured 	and unsecured creditors in business reorganizations and bankruptcy matters. John Hiltz has also represented consumer debtors in complex Chapter 7 and 13 filings. </p>
+      </div>
+      <div class="col-lg-4"> <img src="../assets/img/Kathy.png" width="100%" class="img-responsive">
+        <div id="cufon-area-personname">KATHY WANTCUH</div>
+        <p align="justify"> Kathy Wantuch focuses primarily in the area of consumer and business bankruptcy. She earned her undergraduate degree in accounting at the University of Illinois in 2003, with Honors. Kathy Wantuch graduated cum laude from The John Marshall Law School in 2007, where she served on The John Marshall Law Review. </p>
+      </div>
+      <div class="col-lg-4"> <img src="../assets/img/Blair.png" width="100%" class="img-responsive">
+        <div id="cufon-area-personname">BLAIR ZANZIG</div>
+        <p align="justify"> John Hiltz has experience representing financial institutions, as well as a wide range of secured and unsecured creditors in business reorganizations and bankruptcy matters. John Hiltz has also represented consumer debtors in complex Chapter 7 and 13 filings. </p>
+      </div>
+    </div>
+  </section>
+</div>
+
+
+<footer style="background-color:#404041; min-height:30px;" >
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8">
+        <div class="footer-left"> @2013. Hiltz & Wantuch LLC. All right reserved. We are a debt relief agency. we help people file for bankrutcy relief under the bankrutcy code. </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="footer-right"> Website design by Fresh Design Studios</div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="../assets/js/jquery-1.11.1.js"></script> 
+<script src="../assets/js/bootstrap.js"></script> 
+<script src="../assets/js/cufon-yui.js"></script> 
+<script src="../assets/js/Trajan_Pro_400-Trajan_Pro_700.font.js"></script> 
+<script src="../assets/js/superfish.js"></script> 
+<script src="../assets/js/hoverIntent.js"></script> 
+<script>
+		jQuery(document).ready(function(){
+			jQuery('ul.sf-menu').superfish();
+		});
+</script> 
+<script type="text/javascript">
+			Cufon.replace('#cufon-area-logotext, #cufon-area-personname, #cufon-area-rightmenu, #cufon-area-logo21');
+    </script>
+</body>
+</html>
