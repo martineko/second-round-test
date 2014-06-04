c3c96a8f37b4db909d18b6a567e8851ead068b9b
martineko <martin.e.setia@gmail.com>
2014-06-05 02:08:59 +0700

    Create wptheme-index.php

---
+++ wptheme-index.php
@@ -0,0 +1,67 @@
+<?php get_header(); ?>
+
+<section class="menu">
+    <div class="row no-gutter">
+      <div class="col-lg-8">
+        <div id="mycarousel" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#mycarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#mycarousel" data-slide-to="1"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="item active"> <a href="#" target="_blank"><img src="../wp-content/themes/HWZlaw/assets/img/slide1.jpg" class="img-responsive" style="width:100%;"></a> </div>
+            <div class="item"> <a href="#" target="_blank"><img src="../wp-content/themes/HWZlaw/assets/img/slide2.jpg"  class="img-responsive" style="width:100%;"></a> </div>
+            <div class="item"> <a href="#" target="_blank"><img src="../wp-content/themes/HWZlaw/assets/img/slide3.jpg"  class="img-responsive" style="width:100%;"></a> </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="menu">
+          <div class="middle-menu">
+            <ul id="menu-middle-menu" class="middle-nav">
+              <li id="menu-item-1" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-1">
+                <div id="cufon-area-rightmenu"> <a href="#">INDIVIDUAL BANKRUPTCY</a> </div>
+              </li>
+              <li id="menu-item-2" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-2">
+                <div id="cufon-area-rightmenu"><a href="#">BUSSINES BANKRUTCY</a></div>
+              </li>
+              <li id="menu-item-3" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-3">
+                <div id="cufon-area-rightmenu"><a href="#">COMERCIAL LIGITATION</a></div>
+              </li>
+              <li id="menu-item-4" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-4">
+                <div id="cufon-area-rightmenu"><a href="#">TAXZYTION</a></div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  
+  <section class="middle-text">
+    <div class="row">
+      <div class="col-lg-4 text-middle2"> <div id="cufon-area-logo21">CUSTOMIZED ADVICE </div></div>
+      <div class="col-lg-8 text-middle2"><div id="cufon-area-logo21">LEGAL FINANCIAL SOLUTION FOR BUSSINES </div></div>
+    </div>
+  </section>
+  
+  
+  <section class="Person">
+    <div class="row">
+      <div class="col-lg-4"> <img src="../wp-content/themes/HWZlaw/assets/img/John.png" width="100%" class="img-responsive">
+        <div id="cufon-area-personname">JOHN F. HILTZ</div>
+        <p  align="justify"> John Hiltz has experience representing financial institutions, as well as a wide range of secured 	and unsecured creditors in business reorganizations and bankruptcy matters. John Hiltz has also represented consumer debtors in complex Chapter 7 and 13 filings. </p>
+      </div>
+      <div class="col-lg-4"> <img src="../wp-content/themes/HWZlaw/assets/img/Kathy.png" width="100%" class="img-responsive">
+        <div id="cufon-area-personname">KATHY WANTCUH</div>
+        <p align="justify"> Kathy Wantuch focuses primarily in the area of consumer and business bankruptcy. She earned her undergraduate degree in accounting at the University of Illinois in 2003, with Honors. Kathy Wantuch graduated cum laude from The John Marshall Law School in 2007, where she served on The John Marshall Law Review. </p>
+      </div>
+      <div class="col-lg-4"> <img src="../wp-content/themes/HWZlaw/assets/img/Blair.png" width="100%" class="img-responsive">
+        <div id="cufon-area-personname">BLAIR ZANZIG</div>
+        <p align="justify"> John Hiltz has experience representing financial institutions, as well as a wide range of secured and unsecured creditors in business reorganizations and bankruptcy matters. John Hiltz has also represented consumer debtors in complex Chapter 7 and 13 filings. </p>
+      </div>
+    </div>
+  </section>
+</div>
+<?php get_footer(); ?>
